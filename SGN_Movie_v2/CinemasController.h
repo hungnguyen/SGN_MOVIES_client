6e48e182d2d80b2f91a35248f53a2d5e4ba9c3fd
TATABIM <nguyenquanghuy1990@gmail.com>
2012-07-24 16:21:36 +0700

    commit 1

---
+++ SGN_Movie_v2/CinemasController.h
@@ -0,0 +1,13 @@
+//
+//  CinemaController.h
+//  SGN_Movie_v2
+//
+//  Created by vnicon on 7/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CinemaController : UIViewController
+
+@end
