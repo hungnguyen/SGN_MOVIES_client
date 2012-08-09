852e80a781cb6b3c5506ebb30c554eca500e8d95
Hung Nguyen <hung.nguyenanh@techpropulsionlabs.com>
2012-08-09 15:13:51 +0700

    31010: Showtimes
    - SGNComboView (new file of SGNCinemaMovieCell): load custom view with xib file, without using code in its owner controller.

--- SGN_Movie_v2/iPhone/Views/Show Times/SGNComboView.h
+++ SGN_Movie_v2/iPhone/Views/Show Times/SGNComboView.h
@@ -9,13 +9,15 @@
 #import <UIKit/UIKit.h>
 #import "HJCache.h"
 
-@interface SGNCinemaMovieCell : UIView
+@class SGNComboView;
+@interface SGNComboView : UIView
 
+@property (strong, nonatomic) IBOutlet UIView *mainView;
 @property (strong, nonatomic) IBOutlet HJManagedImageV *cinemaImage;
 @property (strong, nonatomic) IBOutlet HJManagedImageV *movieImage;
 @property (strong, nonatomic) IBOutlet UILabel *cinemaName;
 @property (strong, nonatomic) IBOutlet UILabel *movieTitle;
 
 - (void)fillWithCinema:(NSArray*)cinemaObj andMovie:(NSArray*)movieObj;
-
+- (id)initWithNibName:(NSString*)nibNameOrNil;
 @end
